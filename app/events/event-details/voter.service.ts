57c7ba65b9dabaa12ba8a9a1d005283318d1e547
Rupam Roy <Rupam.Roy@genworth.com>
2017-06-11 16:23:20 -0400

    persist votes to the server

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@angular/core';
 import { ISession } from "../index";
+import { Http, Headers, Response, ResponseOptions } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/Rx'
 
 @Injectable()
 export class VoterService {
-    constructor() { }
+    constructor(private http: Http) { }
 
-    deleteVoter(session: ISession, voterName: string) {
+    deleteVoter(eventId: number, session: ISession, voterName: string) {
         session.voters = session.voters.filter(voter => voter !== voterName);
+
+        let url = `api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+
+        return this.http.delete(url).catch(this.handleError).subscribe();
     }
 
-    addVoter(session: ISession, voterName: string) {
+    addVoter(eventId: number, session: ISession, voterName: string) {
         session.voters.push(voterName);
+
+        let headers = new Headers({ "Content-Type": "application/json" });
+        let options = new ResponseOptions({ headers: headers });
+        let url = `api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+
+        return this.http.post(url, JSON.stringify({}), options).map((response: Response) => {
+            return response.json();
+        }).catch(this.handleError).subscribe();
     }
 
     userHasVoted(session: ISession, voterName: string) {
         return session.voters.some(voter => voter === voterName);
+    }
+
+    handleError(err: Response) {
+        return Observable.throw(err.statusText);
     }
 }
